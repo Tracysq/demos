5b988fe546f786c11b75bc75d552bcdd7be6b2d6
Tracy Sun <kiki20100908@163.com>
2018-09-18 16:30:26 +0800

    load-more
    ajax 加载更多

---
+++ load-more/router.js
@@ -0,0 +1,12 @@
+app.get('/loadMore', function(req, res) {
+  var index = req.query.index;
+  var length = req.query.length;
+  var data = []
+  for(var i = 0; i < length; i++){
+    data.push('新闻' + (parseInt(index) + i))
+  }
+  setTimeout(function(){
+    res.send(data)
+  }, 5000)
+  // res.send(data)
+})
